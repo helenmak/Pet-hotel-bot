4c1f4f09c2171b6b3868003be295c297dcd54b36
Helen <elenamak@ukr.net>
2023-10-09 23:57:23 +0300

    log env

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,5 +1,6 @@
 const env = process.env.NODE_ENV || 'production'
 
+console.log('env is: ', env)
 const config = await new Promise((res) => import(`./${env}.js`)
     .then((config) => res(config.default)))
 
